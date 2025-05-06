fb2994b84043e6bfdf86c5bc9ebadcddccc005a0
Emavilab <ea3192571@gmail.com>
2025-05-06 13:56:42 -0600

    conexion a base de datos al inicio sesion

---
+++ login.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(login))
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        txtusuario = New TextBox()
+        txtcontraseña = New TextBox()
+        accederbtn = New Button()
+        PictureBox2 = New PictureBox()
+        salirbtn = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(-1, 2)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(304, 378)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Stencil", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(363, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(180, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "INICIAR SESIÓN"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Stencil", 12F, FontStyle.Bold)
+        Label2.Location = New Point(324, 163)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(84, 19)
+        Label2.TabIndex = 2
+        Label2.Text = "USUARIO"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Stencil", 12F, FontStyle.Bold)
+        Label3.Location = New Point(324, 235)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(119, 19)
+        Label3.TabIndex = 3
+        Label3.Text = "CONTRASEÑA"
+        ' 
+        ' txtusuario
+        ' 
+        txtusuario.Location = New Point(324, 185)
+        txtusuario.Name = "txtusuario"
+        txtusuario.Size = New Size(179, 23)
+        txtusuario.TabIndex = 4
+        ' 
+        ' txtcontraseña
+        ' 
+        txtcontraseña.Location = New Point(324, 257)
+        txtcontraseña.Name = "txtcontraseña"
+        txtcontraseña.Size = New Size(179, 23)
+        txtcontraseña.TabIndex = 5
+        ' 
+        ' accederbtn
+        ' 
+        accederbtn.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        accederbtn.Image = CType(resources.GetObject("accederbtn.Image"), Image)
+        accederbtn.ImageAlign = ContentAlignment.MiddleLeft
+        accederbtn.Location = New Point(324, 301)
+        accederbtn.Name = "accederbtn"
+        accederbtn.Size = New Size(116, 51)
+        accederbtn.TabIndex = 6
+        accederbtn.Text = "ACCEDER"
+        accederbtn.TextAlign = ContentAlignment.MiddleRight
+        accederbtn.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(410, 37)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(93, 92)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 8
+        PictureBox2.TabStop = False
+        ' 
+        ' salirbtn
+        ' 
+        salirbtn.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        salirbtn.Image = CType(resources.GetObject("salirbtn.Image"), Image)
+        salirbtn.ImageAlign = ContentAlignment.MiddleLeft
+        salirbtn.Location = New Point(457, 301)
+        salirbtn.Name = "salirbtn"
+        salirbtn.Size = New Size(96, 51)
+        salirbtn.TabIndex = 9
+        salirbtn.Text = "SALIR"
+        salirbtn.TextAlign = ContentAlignment.MiddleRight
+        salirbtn.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(600, 378)
+        Controls.Add(salirbtn)
+        Controls.Add(PictureBox2)
+        Controls.Add(accederbtn)
+        Controls.Add(txtcontraseña)
+        Controls.Add(txtusuario)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtusuario As TextBox
+    Friend WithEvents txtcontraseña As TextBox
+    Friend WithEvents accederbtn As Button
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents salirbtn As Button
+
+End Class
---
+++ login.vb
@@ -0,0 +1,49 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class login
+    Private Sub accederbtn_Click(sender As Object, e As EventArgs) Handles accederbtn.Click
+        ' Cadena de conexión para MySQL
+        Dim connectionString As String = "Server=127.0.0.1;Database=turborepuestodb;Uid=root;Pwd=;"
+        Dim query As String = "SELECT COUNT(*) FROM usuarios WHERE nombre_usuario = @usuario AND contraseña = @contraseña"
+
+        ' Obtener valores de los campos
+        Dim usuario As String = txtusuario.Text.Trim()
+        Dim contraseña As String = txtcontraseña.Text.Trim()
+
+        ' Validación de campos vacíos
+        If String.IsNullOrEmpty(usuario) OrElse String.IsNullOrEmpty(contraseña) Then
+            MessageBox.Show("Por favor, complete todos los campos.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Conexión y validación
+        Using connection As New MySqlConnection(connectionString)
+            Try
+                connection.Open()
+                Using command As New MySqlCommand(query, connection)
+                    ' Parámetros para evitar inyección SQL
+                    command.Parameters.AddWithValue("@usuario", usuario)
+                    command.Parameters.AddWithValue("@contraseña", contraseña)
+
+                    ' Ejecutar consulta
+                    Dim count As Integer = Convert.ToInt32(command.ExecuteScalar())
+
+                    If count > 0 Then
+                        MessageBox.Show("Inicio de sesión exitoso.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        Dim siguienteFormulario As New menu()
+                        siguienteFormulario.Show()
+                        Me.Hide()
+                    Else
+                        MessageBox.Show("Usuario o contraseña incorrectos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("Error al conectar con la base de datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub salirbtn_Click(sender As Object, e As EventArgs) Handles salirbtn.Click
+        Me.Close()
+    End Sub
+End Class
